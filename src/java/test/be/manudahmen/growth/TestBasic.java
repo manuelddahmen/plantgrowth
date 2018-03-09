63c0fb42e5742c1e41776384f89164d6e9f0d805
Manuel Dahmen <manueldahmen.smoke@gmail.com>
2018-03-09 14:25:09 +0100

    Before all tests: cleaning testResults directory

--- src/java/test/be/manudahmen/growth/TestBasic.java
+++ src/java/test/be/manudahmen/growth/TestBasic.java
@@ -64,6 +64,22 @@
 
 /*
  * This file is part of Plants-Growth-2
+ *     Plants-Growth-2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Plants-Growth-2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This file is part of Plants-Growth-2
  *     Foobar is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
@@ -102,6 +118,11 @@ import be.manudahmen.growth.SymbolSequence;
 import junit.framework.TestCase;
 
 public class TestBasic extends TestCase {
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
     public void generate1(LSystem lSystem) {
         lSystem.applyRules();
     }
--- src/java/test/be/manudahmen/growth/TestRotation2_methodeBis.java
+++ src/java/test/be/manudahmen/growth/TestRotation2_methodeBis.java
@@ -62,6 +62,22 @@
  *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of Plants-Growth-2
+ *     Plants-Growth-2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Plants-Growth-2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package be.manudahmen.growth;
 
 import be.manudahmen.empty3.CouleurOutils;
@@ -73,6 +89,11 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class TestRotation2_methodeBis extends TestCaseExtended {
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
 
     private Rotation2 rot = new Rotation2();
 
