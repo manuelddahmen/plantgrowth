4f685351b3ec6bc93f6e8c7811750b3b3a4f9159
Manuel Dahmen <manueldahmen.smoke@gmail.com>
2018-03-14 08:41:12 +0100

    Audio starts

--- src/java/main/be/manudahmen/growth/audio/SoundProductionSystem.java
+++ src/java/main/be/manudahmen/growth/audio/SoundProductionSystem.java
@@ -30,7 +30,27 @@
  *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of Plants-Growth-2
+ *     Plants-Growth-2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Plants-Growth-2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package be.manudahmen.growth.audio;
+
+import be.manudahmen.empty3.BSpline;
+import be.manudahmen.empty3.Point3D;
+import be.manudahmen.empty3.core.nurbs.BSplineCurve;
 
 import javax.sound.sampled.*;
 import java.io.ByteArrayInputStream;
@@ -38,6 +58,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class SoundProductionSystem {
+    public enum Waveform {SIN, RECT, DECAY, TRI}
+
+    ;
     private boolean fileOutput;
     private File outWavFile;
     private SourceDataLine sdl;
@@ -45,6 +68,7 @@ public class SoundProductionSystem {
     byte[] buffer;
     AudioInputStream audioInputStream;
     private int buffIdx = 0;
+    private final int samplerate = 44100;
 
     public SoundProductionSystem(float secondsFile, File outWavFile) {
         this();
@@ -70,13 +94,56 @@ public class SoundProductionSystem {
 
     }
 
-    public void playNote(int tone, float durationMs) throws LineUnavailableException {
+    public void playNote(int tone, float durationMs, Waveform type) throws LineUnavailableException {
+
         if (sdl != null && sdl.isOpen()) {
+
             byte[] buf = new byte[4];
+
+/*
+            BSpline bSplineCurve = new BSpline();
+            bSplineCurve.add(new Point3D(0.0, 0.0, 0.0));
+            bSplineCurve.add(new Point3D(0.0, 1.0, durationMs/10));
+            bSplineCurve.add(new Point3D(0.0, 1.0, durationMs/2));
+            bSplineCurve.add(new Point3D(0.0, 1.0, durationMs*3/4.0));
+            bSplineCurve.add(new Point3D(0.0, 0.2, durationMs*7/10.0));
+            bSplineCurve.add(new Point3D(0.0, 0.0, durationMs));
+            bSplineCurve.setDegree(6);
+*/
+
+            BSplineCurve bSplineCurve = new BSplineCurve();
+            bSplineCurve.add(0.0, new Point3D(0.0, 0.0, 0.0));
+            bSplineCurve.add(0.1, new Point3D(0.0, 1.0, durationMs / 10));
+            bSplineCurve.add(0.3, new Point3D(0.0, 1.0, durationMs / 2));
+            bSplineCurve.add(0.0, new Point3D(0.0, 1.0, durationMs * 3 / 4.0));
+            bSplineCurve.add(0.5, new Point3D(0.0, 0.2, durationMs * 7 / 10.0));
+            bSplineCurve.add(0.1, new Point3D(0.0, 0.0, durationMs));
+            System.out.println(bSplineCurve.calculerPoint3D(0.1));
+
             for (int i = 0; i < durationMs * 44100 / 1000.0; i++) {
                 double angle = i / (44100.0 / calculateNoteFrequency(tone)) * 2.0 * Math.PI;
-                buf[0] = (byte) (Math.sin(angle) * 100);
-                short a = (short) (Math.sin(angle) * 32767);  //32767 - max value for sample to take (-32767 to 32767)
+
+
+                double facteurAmpl = bSplineCurve.calculerPoint3D(0.001 * i).getY();
+
+                double ampl = 32767;//*facteurAmpl;
+
+                short a;
+                switch (type) {
+                    case SIN: // SIN
+                        a = (short) (Math.sin(angle) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                        break;
+                    case RECT: // RECT
+                        a = (short) (Math.signum(Math.sin(angle)) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                    case DECAY: // DECAY LINEAR
+                        a = (short) ((1 - angle / 2 * Math.PI) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                    case TRI: // DECAY LINEAR
+                        a = (short) ((1 - Math.abs(angle / 2 * Math.PI) * ampl));  //32767 - max value for sample to take (-32767 to 32767)
+                    default: // SIN
+                        a = (short) (Math.sin(angle) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                        break;
+
+                }
                 buf[0] = (byte) (a & 0xFF); //write 8bits ________WWWWWWWW out of 16
                 buf[1] = (byte) (a >> 8); //write 8bits WWWWWWWW________ out of 16
                 buf[2] = (byte) (a & 0xFF); //write 8bits ________WWWWWWWW out of 16
@@ -89,10 +156,11 @@ public class SoundProductionSystem {
                 buffer[buffIdx++] = buf[1];
                 buffer[buffIdx++] = buf[2];
                 buffer[buffIdx++] = buf[3];
+                if (buffIdx >= buffer.length) {
+                    end();
+                }
             }
-            if (buffIdx >= buffer.length) {
-                end();
-            }
+
             //sdl.write(buf, 0, 1);
 
         }
@@ -120,5 +188,38 @@ public class SoundProductionSystem {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public int equiv(String noteAnglaise) {
+        assert noteAnglaise != null;
+        int tone = 0;
+        noteAnglaise = noteAnglaise.toUpperCase();
+        switch (noteAnglaise.charAt(0)) {
+            case 'C'://DO
+                tone += 3 - 12;
+                break;
+            case 'D'://RE
+                tone += 5 - 12;
+                break;
+            case 'E'://MI
+                tone += 7 - 12;
+                break;
+            case 'F'://FA
+                tone += 8 - 12;
+                break;
+            case 'G'://SOL
+                tone += 10 - 12;
+                break;
+            case 'A'://LA
+                tone += 0;
+                break;
+            case 'B'://SI
+                tone += 2;
+                break;
+        }
+
+        tone += ((Integer.parseInt("" + noteAnglaise.charAt(1))) - 4) * 12;
+
+        return tone;
     }
 }
--- src/java/test/be/manudahmen/growth/graphics/sound/test/TestSound.java
+++ src/java/test/be/manudahmen/growth/graphics/sound/test/TestSound.java
@@ -14,22 +14,6 @@
  *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of Plants-Growth-2
- *     Plants-Growth-2 is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     Plants-Growth-2 is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package be.manudahmen.growth.graphics.sound.test;
 
 import be.manudahmen.growth.TestCaseExtended;
@@ -45,7 +29,7 @@ public class TestSound extends TestCaseExtended {
                     getUniqueFilenameForProduction("testResults", "sounds", "wav"));
             for (int tone = 0; tone < 12; tone++) {
                 System.out.println("note" + tone);
-                soundProductionSystem.playNote(tone, 1000);
+                soundProductionSystem.playNote(tone, 1000, SoundProductionSystem.Waveform.SIN);
             }
             soundProductionSystem.end();
         } catch (LineUnavailableException e) {
@@ -63,7 +47,7 @@ public class TestSound extends TestCaseExtended {
             try {
                 for (int tone = 0; tone < 8 * 12; tone += 12) {
                     System.out.println("note" + tone);
-                    soundProductionSystem.playNote(tone, 1000);
+                    soundProductionSystem.playNote(tone, 1000, SoundProductionSystem.Waveform.SIN);
                 }
             } catch (LineUnavailableException e) {
                 e.printStackTrace();
@@ -71,6 +55,20 @@ public class TestSound extends TestCaseExtended {
             soundProductionSystem.end();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void testNotationAnglaiseNote() {
+        String[] notes = new String[]{"C4", "D4", "E4", "F4", "G4", "A4", "B4"};
+        SoundProductionSystem soundProductionSystem = new SoundProductionSystem();
+        for (String note : notes) {
+            try {
+                int tone = soundProductionSystem.equiv(note);
+                System.out.println(note + " " + tone);
+                soundProductionSystem.playNote(tone, 1000.0f, SoundProductionSystem.Waveform.SIN);
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
