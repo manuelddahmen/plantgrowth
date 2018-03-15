8d2ca9ae0159658b653d9750358992b6ca125d0e
Manuel Dahmen <manueldahmen.smoke@gmail.com>
2018-03-15 09:06:04 +0100

    Error in move

--- src/java/test/be/manudahmen/growth/graphics/test/Turtle2DTest.java
+++ src/java/test/be/manudahmen/growth/graphics/test/Turtle2DTest.java
@@ -80,18 +80,34 @@
 
 /*
  * This file is part of Plants-Growth-2
- *     Plants-Growth-2 is free software: you can redistribute it and/or modify
+ *     Foobar is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  *
- *     Plants-Growth-2 is distributed in the hope that it will be useful,
+ *     Foobar is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
  *
  *     You should have received a copy of the GNU General Public License
- *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This file is part of Plants-Growth-2
+ *     Foobar is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Foobar is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -126,97 +142,67 @@
  *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package be.manudahmen.growth;
+package be.manudahmen.growth.graphics.test;
 
-import be.manudahmen.growth.LSystem;
-import be.manudahmen.growth.Symbol;
-import be.manudahmen.growth.SymbolSequence;
+import be.manudahmen.empty3.Point2D;
+import be.manudahmen.empty3.Point3D;
+import be.manudahmen.growth.graphics.Turtle2D;
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
 
-public class TestBasic extends TestCase {
-    @Override
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class Turtle2DTest extends TestCase {
+
+    @Before
     public void setUp() throws Exception {
         super.setUp();
     }
 
-    public void generate1(LSystem lSystem) throws NotWellFormattedSystem {
-        lSystem.applyRules();
+    @After
+    public void tearDown() throws Exception {
     }
 
-    public void test0() {
-        assertTrue(new SymbolSequence(new Symbol('A')).equals(new SymbolSequence(new Symbol('A'))));
-    }
-
-    public void testA() throws NotWellFormattedSystem {
-
-        LSystem lSystem = new LSystem();
-        lSystem.init();
-        SymbolSequence a1 = new SymbolSequence(new Symbol('A'));
-        SymbolSequence a = new SymbolSequence(new Symbol('A'));
-        lSystem.addRule(a, a1);
-
-
-        lSystem.setCurrentSymbols("A");
-
-        System.out.println(lSystem);
-
-        lSystem.applyRules();
-
-
-        System.out.println(lSystem);
-
-        assertTrue(lSystem.getCurrentSymbols().equals(new SymbolSequence(new Symbol('A'))));
-
-    }
-
-    public void generateN(LSystem lSystem, int n) throws NotWellFormattedSystem {
-        SymbolSequence ab = new SymbolSequence();
-        ab.add(new Symbol('A'));
-        ab.add(new Symbol('B'));
-
-        SymbolSequence a1 = new SymbolSequence(new Symbol('A'));
-        SymbolSequence b = new SymbolSequence(new Symbol('B'));
-        SymbolSequence a = new SymbolSequence(new Symbol('A'));
-
-        lSystem.addRule(a, ab);// a -> ab
-        lSystem.addRule(b, a1);// b -> a
-
-        System.out.println("BEFORE" + lSystem.toString());
-        for (int i = 0; i < n; i++) {
-
-            generate1(lSystem);
-
-            //System.out.println("AFTER " + (i + 1) + " PASS : " + lSystem.toString());
+    public void testLineRotate() {
+        BufferedImage bufferedImage = new BufferedImage(1024, 768, BufferedImage.TYPE_INT_RGB);
+        Turtle2D turtle2D = new Turtle2D(bufferedImage);
+        Point2D o = new Point2D(turtle2D.getPosition().getX(),
+                turtle2D.getPosition().getY());
+        assertEqualsPoint3D(turtle2D.getPosition()
+                , new Point2D(0.0, 0.).plus(o).get3D());
+        turtle2D.line(100.0);
+        turtle2D.right(Math.PI / 2);
+        assertEqualsPoint3D(turtle2D.getPosition()
+                , new Point2D(100.0, 0.).plus(o).get3D());
+        turtle2D.line(100.0);
+        turtle2D.right(Math.PI / 2);
+        assertEqualsPoint3D(turtle2D.getPosition()
+                , new Point2D(100.0, -100.).plus(o).get3D());
+        turtle2D.line(100.0);
+        turtle2D.right(Math.PI / 2);
+        assertEqualsPoint3D(turtle2D.getPosition()
+                , new Point2D(0.0, -100.).plus(o).get3D());
+        turtle2D.line(100.0);
+        turtle2D.right(Math.PI / 2);
+        assertEqualsPoint3D(turtle2D.getPosition()
+                , new Point2D(0.0, 0.0).plus(o).get3D());
+        try {
+            ImageIO.write(bufferedImage, "jpg", new java.io.File("testResults/testLineRotate.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
     }
 
-    public void testGenerate2() throws NotWellFormattedSystem {
-        LSystem lSystem = new LSystem();
-        lSystem.init();
-        lSystem.getCurrentSymbols().add(new Symbol('A'));
-        generateN(lSystem, 2);
-        SymbolSequence symbolSequence = new SymbolSequence();
-        symbolSequence.add(new Symbol('A'));
-        symbolSequence.add(new Symbol('B'));
-        symbolSequence.add(new Symbol('A'));
-        assertTrue(lSystem.getCurrentSymbols().equals(symbolSequence));
-    }
-
-    public void testGenerate1() throws NotWellFormattedSystem {
-
-        LSystem lSystem = new LSystem();
-        lSystem.init();
-        lSystem.getCurrentSymbols().add(new Symbol('A'));
-
-        // run
-        generateN(lSystem, 1);
-
-        // Waited for:
-        SymbolSequence symbolSequence = new SymbolSequence();
-        symbolSequence.add(new Symbol('A'));
-        symbolSequence.add(new Symbol('B'));
-
-        // evaluate the answer
-        assertTrue(lSystem.getCurrentSymbols().equals(symbolSequence));
+    public void assertEqualsPoint3D(Point3D x, Point3D y) {
+        for (int i = 0; i < 3; i++) {
+            assertEquals(y.get(i), x.get(i), 0.01);
+        }
     }
 }
--- src/java/test/be/manudahmen/growth/graphics/test/Turtle3D_4Test.java
+++ src/java/test/be/manudahmen/growth/graphics/test/Turtle3D_4Test.java
@@ -46,10 +46,27 @@
  *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package be.manudahmen.growth.graphics;
+/*
+ * This file is part of Plants-Growth-2
+ *     Plants-Growth-2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Plants-Growth-2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package be.manudahmen.growth.graphics.test;
 
 import be.manudahmen.empty3.*;
 import be.manudahmen.growth.TestCaseExtended;
+import be.manudahmen.growth.graphics.Turtle3D_4;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
