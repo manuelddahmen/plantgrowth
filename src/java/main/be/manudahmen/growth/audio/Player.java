9b5eb4e7c52bf26bf6f9b5a30b0bd842f565b0fe
Manuel Dahmen <manueldahmen.smoke@gmail.com>
2018-03-25 12:27:23 +0200

    Now SOUND Begins...

--- src/java/main/be/manudahmen/growth/audio/Player.java
+++ src/java/main/be/manudahmen/growth/audio/Player.java
@@ -78,6 +78,22 @@
  *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of Plants-Growth-2
+ *     Plants-Growth-2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Plants-Growth-2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Plants-Growth-2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package be.manudahmen.growth.audio;
 
 import javafx.application.Platform;
@@ -118,10 +134,11 @@ public class Player extends Thread {
     }
 
     public void playCurrentNotes() {
+        double total = 0;
         double facteurAmpl = 0;
         short a = 0;
         for (Note note : currentNotes) {
-            if (note.getDurationMs() <= note.getTimer().getTimeEllapsedMS()) {
+            if (note.getDurationMs() + timer.getTimeEllapsedMS() <= note.getTimer().getTimeEllapsedMS()) {
 
                 double index = note.getTimer().getTimeEllapsedMS() * 44100 / 1000.0;
 
@@ -133,31 +150,31 @@ public class Player extends Thread {
 
                 switch (note.getWaveform()) {
                     case SIN: // SIN
-                        a += (Math.sin(angle) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                        total += (Math.sin(angle) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
                         break;
                     case RECT: // RECT
-                        a += (Math.signum(Math.sin(angle)) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                        total += (Math.signum(Math.sin(angle)) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
                     case DECAY: // DECAY LINEAR
-                        a += ((1 - angle / 2 * Math.PI) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                        total += ((1 - angle / 2 * Math.PI) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
                     case TRI: // DECAY LINEAR
-                        a += ((1 - Math.abs(angle / 2 * Math.PI) * ampl));  //32767 - max value for sample to take (-32767 to 32767)
+                        total += ((1 - Math.abs(angle / 2 * Math.PI) * ampl));  //32767 - max value for sample to take (-32767 to 32767)
                     default: // SIN
-                        a += (Math.sin(angle) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
+                        total += (Math.sin(angle) * ampl);  //32767 - max value for sample to take (-32767 to 32767)
                         break;
 
                 }
 
             }
         }
-        a /= Math.sqrt(currentNotes.size());
+        total /= Math.sqrt(currentNotes.size() > 0 ? currentNotes.size() : 1);
 
-        short ampl = (short) a;
+        short amplitude = (short) total;
 
-        playBuffer(a);
+        playBuffer(amplitude);
     }
 
     public void playBuffer(short amplitude) {
-        short a = (short) amplitude;
+        short a = amplitude;
         byte[] nextBuffer = new byte[4];
         nextBuffer[0] = (byte) (a & 0xFF); //write 8bits ________WWWWWWWW out of 16
         nextBuffer[1] = (byte) (a >> 8); //write 8bits WWWWWWWW________ out of 16
@@ -165,7 +182,9 @@ public class Player extends Thread {
         nextBuffer[3] = (byte) (a >> 8); //write 8bits WWWWWWWW________ out of 16
         try {
             soundProductionSystem.getLine().write(nextBuffer, 0, 4);
+            //System.out.println("bytes written : 4" + amplitude);
         } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
@@ -216,6 +235,7 @@ public class Player extends Thread {
     }
 
     public List<Note> getCurrentNotes() {
+        System.out.println("notes size: " + currentNotes.size());
         return currentNotes;
     }
 }
